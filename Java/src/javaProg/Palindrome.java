9c96743b789dcfefcb0906fa51e3b74a932a8e8a
kalai153vanan <kalai153vanan@gmail.com>
2022-02-03 03:18:06 -0800

    version1

--- Java/src/javaProg/Palindrome.java
+++ Java/src/javaProg/Palindrome.java
@@ -4,7 +4,11 @@ public class Palindrome {
 
 	public static void main(String[] args) {
 		
+		System.out.println("kalai");
+		System.out.println("kalaiva");
+		System.out.println("kalaivan");
 		System.out.println("kalaivanan");
+		
 	}
 
 }
